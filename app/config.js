7d7dd2f4533d6f15ade064be1ec749c923b63038
oskarhermanowski <oskar.hermanowski@gmail.com>
2025-05-26 23:31:13 +0200

    test11

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    port: 3000,
-    uri: 'mongodb+srv://oskarhermanowski:AIHWdBWaA2p7bKpb@cluster0.fjfhcmn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    port: process.env.PORT || 3000,
+    uri: process.env.MONGODB_URI || 'mongodb+srv://oskarhermanowski:AIHWdBWaA2p7bKpb@cluster0.fjfhcmn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 };
